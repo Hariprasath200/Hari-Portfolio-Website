c5d99d60db11c14226f498b317dc406dac826b0e
Hari <haridhamo3@gmail.com>
2024-03-17 21:17:16 +0530

    Commit message

--- prasath/urls.py
+++ prasath/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+def static_urlpatterns():
+    return static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    
 
 urlpatterns=[
     path('',views.prasath,name="home"),
@@ -7,4 +14,5 @@ urlpatterns=[
     path('certificate',views.certificate,name="certificate"),
     path('project',views.project,name="project"),
     path('contact',views.contact,name="contact"),
-]
+] , static_urlpatterns()
+       
