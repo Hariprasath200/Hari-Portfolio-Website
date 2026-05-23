bd29734187a6e17ebef159be59300f80a6e3af7c
HARIPRASATH S <125465139+Hariprasath200@users.noreply.github.com>
2026-05-23 20:35:37 +0530

    Update urls.py

--- prasath/urls.py
+++ prasath/urls.py
@@ -3,16 +3,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-def static_urlpatterns():
-    return static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('home/', views.prasath, name="home"),
+    path('about/', views.about, name="about"),
+    path('certificate/', views.certificate, name="certificate"),
+    path('project/', views.project, name="project"),
+    path('contact/', views.contact, name="contact"),
+]
 
-    
-
-urlpatterns=[
-    path('/home',views.prasath,name="home"),
-    path('/about',views.about,name="about"),
-    path('certificate',views.certificate,name="certificate"),
-    path('project',views.project,name="project"),
-    path('contact',views.contact,name="contact"),
-] , static_urlpatterns()
-       
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
